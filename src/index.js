aded736b59993db0fec4707c011a6566735e4476
Ayushi Soni <ayushisoni@Ayushis-MacBook-Air.local>
2025-07-08 16:53:27 +0530

    Added backend project setup with ping check

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+import { PORT } from './config/serverConfig.js';
+import apiRouter from './routes/index.js';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cors());
+
+app.use('/api', apiRouter);
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
